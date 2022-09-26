eb9a5ac70a5bf0fe4a3bef46beff5c9f6d982a9a
Erick Puga <erick.puga@hotmail.com>
2022-09-26 10:04:13 -0500

    Run sps1 in pipeline

--- enable-vmInsights.ps1
+++ enable-vmInsights.ps1
@@ -2,6 +2,7 @@
 #az monitor log-analytics workspace get-shared-keys --resource-group $ResourceGroup --workspace-name $WorkspaceName
 #Get-AzOperationalInsightsWorkspaceSharedKey -ResourceGroupName $ResourceGroup -Name $WorkspaceName
 
+Install-Script -Name Install-VMInsights
 
 $WorkspaceId = "c37e78a5-2fd6-4a0e-8e7e-2b46b3946281"
 $SubscriptionId = "8911ade0-2192-425e-a08d-c27be185a83e"
@@ -10,4 +11,4 @@ $WorkspaceName = "wks-insights-test01"
 $WorkspaceKey = Get-AzOperationalInsightsWorkspaceSharedKey -ResourceGroupName $ResourceGroup -Name $WorkspaceName
 
 
-.\Install-VMInsights.ps1 -WorkspaceId $WorkspaceId -WorkspaceKey $WorkspaceKey.PrimarySharedKey -SubscriptionId $SubscriptionId -WorkspaceRegion eastus2 -ResourceGroup $ResourceGroup
+#.\Install-VMInsights.ps1 -WorkspaceId $WorkspaceId -WorkspaceKey $WorkspaceKey.PrimarySharedKey -SubscriptionId $SubscriptionId -WorkspaceRegion eastus2 -ResourceGroup $ResourceGroup
