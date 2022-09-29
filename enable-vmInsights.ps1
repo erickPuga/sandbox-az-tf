508ad30065bd266add41fdaf8bf13890a50e01d3
Erick Puga <erick.puga@hotmail.com>
2022-09-29 12:42:58 -0500

    Executing Install VmInsights

--- enable-vmInsights.ps1
+++ enable-vmInsights.ps1
@@ -2,16 +2,17 @@
 #az monitor log-analytics workspace get-shared-keys --resource-group $ResourceGroup --workspace-name $WorkspaceName
 #Get-AzOperationalInsightsWorkspaceSharedKey -ResourceGroupName $ResourceGroup -Name $WorkspaceName
 
-#Install-Script -Name Install-VMInsights
+Install-Script -Name Install-VMInsights -Force
+Install-Module -Name Az.OperationalInsights -Force
 
 $WorkspaceId = "c37e78a5-2fd6-4a0e-8e7e-2b46b3946281"
 $SubscriptionId = "8911ade0-2192-425e-a08d-c27be185a83e"
 $ResourceGroup = "rg-test-insights"
 $WorkspaceName = "wks-insights-test01"
-#$WorkspaceKey = Get-AzOperationalInsightsWorkspaceSharedKey -ResourceGroupName $ResourceGroup -Name $WorkspaceName
+$WorkspaceKey = Get-AzOperationalInsightsWorkspaceSharedKey -ResourceGroupName $ResourceGroup -Name $WorkspaceName
 
 
-#.\Install-VMInsights.ps1 -WorkspaceId $WorkspaceId -WorkspaceKey $WorkspaceKey.PrimarySharedKey -SubscriptionId $SubscriptionId -WorkspaceRegion eastus2 -ResourceGroup $ResourceGroup
+.\Install-VMInsights.ps1 -WorkspaceId $WorkspaceId -WorkspaceKey $WorkspaceKey.PrimarySharedKey -SubscriptionId $SubscriptionId -WorkspaceRegion eastus2 -ResourceGroup $ResourceGroup
 
 
 #$WorkspaceId = "1c1ab36e-1c45-41e6-bc37-4474b555a93f"
